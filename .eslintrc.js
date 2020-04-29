1f628164ba015160e150eb9b3397bb5c91db6cbf
AhmedSafi97 <ahmadhsafi1997@gmail.com>
2020-04-30 00:19:58 +0300

    re-setup eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,44 @@
 module.exports = {
+  extends: ["airbnb", "prettier", "prettier/react"],
+  // babel-eslint parser is used to support experimental features not supported in ESLint itself yet
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaVersion: 8,
+    ecmaFeatures: {
+      impliedStrict: true, //enable global strict mode (if ecmaVersion is 5 or greater)
+    },
+  },
+  root: true,
   env: {
     browser: true,
-    es6: true,
     node: true,
+    jest: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    "airbnb", "airbnb/hooks",
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: [
-    "prettier",
-  ],
   rules: {
-    "prettier/prettier": "error"
+    //  allow .js extensions for JSX.
+    "react/state-in-constructor": 0,
+    "react/jsx-filename-extension": [
+      1,
+      {
+        extensions: [".js", ".jsx"],
+      },
+    ],
+    quotes: [
+      2,
+      "single",
+      {
+        avoidEscape: true, // allows strings to use single-quotes or double-quotes so long as the string contains a quote that would have to be escaped otherwise
+        allowTemplateLiterals: true, // allows strings to use backticks
+      },
+    ],
+    // configure the prettier plugin
+    "prettier/prettier": [
+      "error",
+      {
+        trailingComma: "es5",
+        singleQuote: true,
+      },
+    ],
   },
+  plugins: ["react", "prettier"],
 };
--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>
-        Flip Card Game
-      </h1>
+      <h1>Flip Card Game</h1>
     </div>
   );
 }
