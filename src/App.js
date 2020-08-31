fd5a84bdbd54e216eec52951bc718578e1f9632e
AhmedSafi97 <ahmadhsafi1997@gmail.com>
2020-08-31 19:26:15 +0300

    use react router to navigate between pages

--- src/App.js
+++ src/App.js
@@ -1,68 +1,28 @@
-/* eslint-disable react/no-unescaped-entities */
-import React, { useState } from 'react';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 
-import Board from './Components/Board';
+import { Home, Score } from './Pages';
 
 const App = () => {
-  const [start, setStart] = useState(false);
-  const [level, setLevel] = useState(null);
-  const [error, setError] = useState(null);
-
-  const gameStart = () => {
-    if (level) setStart(true);
-    else setError('please choose level');
-  };
-
-  const selectLevel = (e) => setLevel(e.target.value);
-
   return (
-    <div className="container">
-      <h1>Flip Card Game</h1>
-      {!start && (
-        <div>
-          <p>Do you have a strong memory ? let's see ...</p>
-          <p>click on a card to flip it and try to make a match</p>
-          <div style={error && { border: '2px red solid' }}>
-            <p>Please select level:</p>
-            <label htmlFor="easy">
-              <input
-                type="radio"
-                id="easy"
-                name="level"
-                value="20"
-                onChange={selectLevel}
-              />
-              Easy
-            </label>
-            <label htmlFor="medium">
-              <input
-                type="radio"
-                id="medium"
-                name="level"
-                value="30"
-                onChange={selectLevel}
-              />
-              Medium
-            </label>
-            <label htmlFor="hard">
-              <input
-                type="radio"
-                id="hard"
-                name="level"
-                value="40"
-                onChange={selectLevel}
-              />
-              Hard
-            </label>
-          </div>
-          <button onClick={gameStart} type="button" className="game-start-btn">
-            Game Start
-          </button>
-        </div>
-      )}
-      <hr />
-      {start && <Board level={Number(level)} />}
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/score">
+          <Score />
+        </Route>
+        <Route path="*">
+          <Redirect to="/" />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
