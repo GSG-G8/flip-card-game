506bad47fd4185c0c9a19539812e7b623e112446
AhmedSafi97 <ahmadhsafi1997@gmail.com>
2020-09-01 09:21:38 +0300

    create game over component to indicate the end of the game and save the players' scores

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -4,19 +4,20 @@ import propTypes from 'prop-types';
 import Card from './Card';
 import { names, cardsGenerator } from './CardsInfo';
 import useTimer from './useTimer';
+import GameOver from './GameOver';
 
-const Board = ({ level }) => {
+const Board = ({ level, cardsNum }) => {
   const [cards, setCards] = useState([]);
   const [flipped, setFlipped] = useState([]);
   const [eliminated, setEliminated] = useState([]);
   const [disabled, setDisabled] = useState(false);
   const [gameOver, setGameOver] = useState(false);
 
-  const { roundTime, setRoundTime } = useTimer(gameOver);
+  const { roundTime } = useTimer(gameOver);
 
   useEffect(() => {
-    setCards(cardsGenerator(names, level));
-  }, [level]);
+    setCards(cardsGenerator(names, cardsNum));
+  }, [cardsNum]);
 
   const clear = () => {
     setFlipped([]);
@@ -32,13 +33,6 @@ const Board = ({ level }) => {
   const success = (id) => {
     setEliminated([...eliminated, flipped[0], id]);
     clear();
-  };
-
-  const playAgain = () => {
-    clear();
-    setGameOver(false);
-    setRoundTime(0);
-    setCards(cardsGenerator(names, level));
   };
 
   const sameCard = (id) => flipped.includes(id);
@@ -59,44 +53,39 @@ const Board = ({ level }) => {
     else setTimeout(() => clear(), 1000);
   };
 
-  if (eliminated.length === level) {
+  if (eliminated.length === cardsNum) {
     setEliminated([]);
     setGameOver(true);
   }
 
   return (
     <div>
-      {gameOver ? (
-        <div>
-          <p>Congratulation , you won</p>
-          <button type="button" className="game-start-btn" onClick={playAgain}>
-            Play Again
-          </button>
-        </div>
-      ) : (
-        <div>
-          <div>{new Date(roundTime * 1000).toISOString().substr(11, 8)}</div>
-          <div className="grid">
-            {cards.map((card) => (
-              <Card
-                key={card.id}
-                id={card.id}
-                name={card.name}
-                flipped={flipped.includes(card.id)}
-                eliminated={eliminated.includes(card.id)}
-                handleClick={handleClick}
-                disabled={disabled}
-              />
-            ))}
-          </div>
-        </div>
+      {gameOver && (
+        <GameOver duration={roundTime} cardsNum={cardsNum} level={level} />
       )}
+      <div>
+        <div>{new Date(roundTime * 1000).toISOString().substr(11, 8)}</div>
+        <div className="grid">
+          {cards.map((card) => (
+            <Card
+              key={card.id}
+              id={card.id}
+              name={card.name}
+              flipped={flipped.includes(card.id)}
+              eliminated={eliminated.includes(card.id)}
+              handleClick={handleClick}
+              disabled={disabled}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
 
 Board.propTypes = {
-  level: propTypes.number.isRequired,
+  level: propTypes.string.isRequired,
+  cardsNum: propTypes.number.isRequired,
 };
 
 export default Board;
